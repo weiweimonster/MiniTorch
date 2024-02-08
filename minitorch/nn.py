507b1f4e211195ee4c25f28f53c1c6b86a054f38
weiweimonster <weiweimonster1130@gmail.com>
2024-02-07 21:37:57 -0500

    add all

---
+++ minitorch/nn.py
@@ -0,0 +1,175 @@
+from typing import Tuple
+
+from . import operators
+from .autodiff import Context
+from .fast_ops import FastOps
+from .tensor import Tensor
+from .tensor_functions import Function, rand, tensor
+
+
+def tile(input: Tensor, kernel: Tuple[int, int]) -> Tuple[Tensor, int, int]:
+    """
+    Reshape an image tensor for 2D pooling
+
+    Args:
+        input: batch x channel x height x width
+        kernel: height x width of pooling
+
+    Returns:
+        Tensor of size batch x channel x new_height x new_width x (kernel_height * kernel_width) as well as the new_height and new_width value.
+    """
+
+    batch, channel, height, width = input.shape
+    kh, kw = kernel
+    assert height % kh == 0
+    assert width % kw == 0
+    # TODO: Implement for Task 4.3.
+    # raise NotImplementedError("Need to implement for Task 4.3")
+    input = (input.contiguous().view(batch, channel, height, int(width / kw), kw).permute(0, 1, 3, 2, 4))
+    input = input.contiguous().view(batch, channel, int(width / kw), int(height / kh), kh * kw)
+    return (input, int(height / kh), int(width / kw))
+
+def avgpool2d(input: Tensor, kernel: Tuple[int, int]) -> Tensor:
+    """
+    Tiled average pooling 2D
+
+    Args:
+        input : batch x channel x height x width
+        kernel : height x width of pooling
+
+    Returns:
+        Pooled tensor
+    """
+    batch, channel, height, width = input.shape
+    # TODO: Implement for Task 4.3.
+    # raise NotImplementedError("Need to implement for Task 4.3")
+    input, h, w = tile(input, kernel)
+    return input.mean(4).view(batch, channel, h, w)
+
+max_reduce = FastOps.reduce(operators.max, -1e9)
+
+
+def argmax(input: Tensor, dim: int) -> Tensor:
+    """
+    Compute the argmax as a 1-hot tensor.
+
+    Args:
+        input : input tensor
+        dim : dimension to apply argmax
+
+
+    Returns:
+        :class:`Tensor` : tensor with 1 on highest cell in dim, 0 otherwise
+
+    """
+    out = max_reduce(input, dim)
+    return out == input
+
+
+class Max(Function):
+    @staticmethod
+    def forward(ctx: Context, input: Tensor, dim: Tensor) -> Tensor:
+        "Forward of max should be max reduction"
+        # TODO: Implement for Task 4.4.
+        # raise NotImplementedError("Need to implement for Task 4.4")
+        ctx.save_for_backward(input, int(dim.item()))
+        return max_reduce(input, int(dim.item()))
+
+    @staticmethod
+    def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, float]:
+        "Backward of max should be argmax (see above)"
+        # TODO: Implement for Task 4.4.
+        # raise NotImplementedError("Need to implement for Task 4.4")
+        input, dim = ctx.saved_values
+        return grad_output * argmax(input, dim), 0.0
+
+def max(input: Tensor, dim: int) -> Tensor:
+    return Max.apply(input, input._ensure_tensor(dim))
+
+
+def softmax(input: Tensor, dim: int) -> Tensor:
+    r"""
+    Compute the softmax as a tensor.
+
+
+
+    $z_i = \frac{e^{x_i}}{\sum_i e^{x_i}}$
+
+    Args:
+        input : input tensor
+        dim : dimension to apply softmax
+
+    Returns:
+        softmax tensor
+    """
+    # TODO: Implement for Task 4.4.
+    # raise NotImplementedError("Need to implement for Task 4.4")
+    return input.exp() / input.exp().sum(dim=dim)
+
+def logsoftmax(input: Tensor, dim: int) -> Tensor:
+    r"""
+    Compute the log of the softmax as a tensor.
+
+    $z_i = x_i - \log \sum_i e^{x_i}$
+
+    See https://en.wikipedia.org/wiki/LogSumExp#log-sum-exp_trick_for_log-domain_calculations
+
+    Args:
+        input : input tensor
+        dim : dimension to apply log-softmax
+
+    Returns:
+         log of softmax tensor
+    """
+    # TODO: Implement for Task 4.4.
+    # raise NotImplementedError("Need to implement for Task 4.4")
+    # return input - (input - max(input, dim)).exp().sum(dim).log() - max(input, dim)
+    # return input - (input - max(input, dim)).exp().sum(dim).log() - max(input, dim)
+    # t = input.exp()
+    # t = t.sum(dim)
+    # t = t.log()
+    # return input - t
+    m = max(input, dim)
+    t = input - m
+    t = t.exp()
+    t = t.sum(dim)
+    t = t.log()
+    input = input - t - m
+    return input
+
+def maxpool2d(input: Tensor, kernel: Tuple[int, int]) -> Tensor:
+    """
+    Tiled max pooling 2D
+
+    Args:
+        input: batch x channel x height x width
+        kernel: height x width of pooling
+
+    Returns:
+        Tensor : pooled tensor
+    """
+    batch, channel, height, width = input.shape
+    # TODO: Implement for Task 4.4.
+    # raise NotImplementedError("Need to implement for Task 4.4")
+    out, h, w = tile(input, kernel)
+    out = max(out, len(out.shape) - 1)
+    return out.view(batch, channel, h, w)
+
+def dropout(input: Tensor, rate: float, ignore: bool = False) -> Tensor:
+    """
+    Dropout positions based on random noise.
+
+    Args:
+        input : input tensor
+        rate : probability [0, 1) of dropping out each position
+        ignore : skip dropout, i.e. do nothing at all
+
+    Returns:
+        tensor with randoom positions dropped out
+    """
+    # TODO: Implement for Task 4.4.
+    # raise NotImplementedError("Need to implement for Task 4.4")
+    result = input
+    if not ignore:
+        result = input * (rand(input.shape) > rate)
+    return result
